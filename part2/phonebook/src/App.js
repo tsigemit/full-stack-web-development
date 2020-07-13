9611f98fcae8d4d5ae8be77ea350416867579979
tsigemit <tsigabum@gmail.com>
2020-07-13 17:57:33 +0200

    Phonebook step6 retrieve persons data fromthe json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-                                            { name: 'Arto Hellas', number: '040-123456' },
-                                            { name: 'Ada Lovelace', number: '39-44-5323523' },
-                                            { name: 'Dan Abramov', number: '12-43-234345' },
-                                            { name: 'Mary Poppendieck', number: '39-23-6423122' }
-                                            ])   
-
-const [newName, setNewName] = useState('')
-const [newNumber, setNewNumber] = useState('')
-
-const handleOnChangeName = (event) => {
+    const [persons, setPersons] = useState([])  
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    
+    const handleOnChangeName = (event) => {
     setNewName(event.target.value)
 }
 
-const handleOnChangeNumber = (event) => {
+    const handleOnChangeNumber = (event) => {
     setNewNumber(event.target.value)
 }
-
-const addPeson = (event) => {
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    const addPeson = (event) => {
     event.preventDefault()
     const nameObject = {
         name: newName,
