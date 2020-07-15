cd946ccc1e946601346034c63fdfa5fabf657822
tsigemit <tsigabum@gmail.com>
2020-07-15 15:30:37 +0200

    implementations of delete and update phonebooks

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import contacts from './services/persons'
+import contacts from './services/functions'
 
 const App = () => {
     const [persons, setPersons] = useState([])  
@@ -33,13 +32,38 @@ const App = () => {
         number: newNumber,
         id:persons.length+1
     }
-     contacts.create(nameObject)
-    if (persons.find(peson => peson.name === newName))
-        alert(`${newName} is already added to phonebook`)
+
+    if (persons.find(peson => peson.name === newName)){
+        if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+            const replaceNumber = persons.filter(person => person.name === newName)
+            let updatePerson = replaceNumber[0]
+            updatePerson.number = newNumber
+            contacts.update(updatePerson.id, updatePerson).then(newContacts => {
+                setPersons(persons.map(person => person.id !== updatePerson.id ? person : newContacts))
+                alert(`${newName}'s number is successfully updated`)
+            })
+    }
+}
     else
+    {
         setPersons(persons.concat(nameObject))
+        contacts.create(nameObject)
+        
+    }
+   
 }
    
+    const handleDelete = (person) => {
+    if (window.confirm(`Delete ${person.name}`)) {
+            contacts.remove(person.id)
+                .then(() =>(
+                   alert(`${person.name} successfully deleted`)
+                ))
+                .catch(error => (`${person.name} ${error}`))
+            const filtered = persons.filter(pre => pre.id !== person.id)
+            setPersons(filtered)
+        }
+    }
 
     return (
         <div>
@@ -58,7 +82,7 @@ const App = () => {
 
             <h2>Numbers</h2>
 
-            <Persons persons={persons} />
+            <Persons persons={persons} handleDelete={handleDelete} />
         </div>
     )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, handleDelete }) => {
     return (
         <div>
             {persons.map(person => {
                 return (
-                    <p key={person.name}>{person.name} {person.number}</p>)
+                    <p key={person.name}>{person.name} {person.number} <button onClick={() => handleDelete(person)}>delete</button></p>
+                    )
+                    
             }
             )}
         </div>
