dd8ca4630952e2075f40feb6be5a058d7ed092cf
tsigemit <tsigabum@gmail.com>
2020-07-27 11:36:07 +0200

    Added a Procfile.

--- part3/phonebook-frontend/index.js
+++ part3/phonebook-frontend/index.js
@@ -1,111 +1,84 @@
 const express = require('express')
-var morgan = require('morgan')
-
 const app = express()
+
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
-app.use(morgan((tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        req.method === 'POST' ? JSON.stringify(req.body) : ''
-    ].join(' ')
-}))
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2020-01-10T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2020-01-10T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2020-01-10T19:20:14.298Z",
+        important: true
+    }
+]
 
-let persons = [
-        {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-        },
-        {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-        },
-        {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-        },
-        {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122",
-            "id": 4
-        }
-    ]
-
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
-app.get('/info', (req, res) => {
-    const timestamp = new Date().toString();
-    let count =0
-    persons.map(person => {
-        if(person.id)
-           count++;
-    })
-    res.send(`<b>Phonebook has info for ${count} people <br /><br /> ${timestamp}</b>`)
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 })
 
 const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
         : 0
     return maxId + 1
 }
 
-app.post('/api/persons', (request, response) => {
-    var name=request.body.name 
-    var number=request.body.number 
-   const newContact = persons.filter(person => (person.name===name))
-         if(newContact.length>0)
-             return response.status(400).json({
-                 error: 'Name must be unique'
-             })
-        else if (!name || !number) {
-            return response.status(400).json({
-                error: 'Name or number is missing'
-            })
-        }
-    
-    const person = {
-        name,
-        number,
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
         id: generateId(),
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
     } else {
-        response.status(404).end('User doesn\'t exist')
+        response.status(404).end()
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    const checkIfExists = persons.find(person => person.id === id)
-    if (!checkIfExists)
-        response.status(404).end('User doesn\'t exist')
-    else
-    {
-        persons = persons.filter(person => person.id !== id)
-        response.json(persons)
-        response.status(204).end()
-    }
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
